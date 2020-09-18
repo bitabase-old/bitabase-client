e367841128c36e50a19c834cd99045be7be6517f
Mark Wylde <me@markwylde.com>
2020-09-18 21:29:33 +1000

    Increase header height

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  docsUrl: process.env.DOCS_URL || 'http://127.0.0.1:8000',
-  blogUrl: process.env.BLOG_URL || 'http://127.0.0.1:8000/blog',
+  docsUrl: process.env.DOCS_URL || 'http://127.0.0.1:9001',
+  blogUrl: process.env.BLOG_URL || 'http://127.0.0.1:9001/blog',
   apiServerUrl: process.env.API_SERVER_URL || 'http://127.0.0.1:8001/v1'
 };
